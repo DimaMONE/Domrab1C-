370c1997d38aa53961af4949deda0dbbdaee9000
SmaginDima <smaginzdima@gmail.com>
2022-11-27 20:49:27 +0300

    вторая задача

---
+++ n2sem1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Нахождение максимального из трех
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a > b || a > c) max = a;
+else if (b > c) max = b;
+else max = c;
+Console.WriteLine($" Максимальное число равно {max} ");
