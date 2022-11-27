0a8fc3b38739c7b5a7237a324a56fb7598fdc147
SmaginDima <smaginzdima@gmail.com>
2022-11-27 20:40:53 +0300

    первая задача

---
+++ n1sem1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Нахождение максимального из двух чисел
+Console.WriteLine("Введите первое число");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numB = Convert.ToInt32(Console.ReadLine());
+if (numA > numB)
+{
+    Console.WriteLine($"Максимальное равно {numA}");
+}
+else
+{
+    Console.WriteLine($"Максимальное равно {numB}");
+}
