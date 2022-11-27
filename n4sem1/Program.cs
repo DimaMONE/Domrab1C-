4f4ce2311e1ddc761ac04473c25af05e4c2c6f1f
SmaginDima <smaginzdima@gmail.com>
2022-11-27 20:56:46 +0300

    четвертая задача

---
+++ n4sem1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Выводит четные числа от 1 до N
+Console.WriteLine("Введите целое число");
+int n = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+while (i <= n)
+{
+    if (i % 2 == 0)
+        Console.WriteLine(i);
+    i = i + 1;
+}
+Console.WriteLine();
